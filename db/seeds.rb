f2eb60fab3363929f9c0f911bdf0a398692f9909
Erin Mahoney <erinmahoneyphoto@gmail.com>
2015-08-10 15:27:49 -0700

    seed fake data associated with posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+	5.times do 
+		user_params = Hash.new
+  	user_params[:username] = FFaker::Lorem.words(1)
+  	user_params[:first_name] = FFaker::Name.first_name
+  	user_params[:last_name] = FFaker::Name.last_name
+  	user_params[:email] = FFaker::Internet.email
+  	user_params[:password] = FFaker::Lorem.words(2).join
+  	user_params[:password_confirmation] = user_params[:password]
+  	@user = User.create(user_params)
+  end
+

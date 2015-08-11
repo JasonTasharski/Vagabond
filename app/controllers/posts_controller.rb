fb3faf3aaebd2d5269dde4b1dcec246e3b74a90e
henry-yi <yoonho.yi88@gmail.com>
2015-08-10 17:01:30 -0700

    set routes for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+	def index #DONT REALLY NEED... yet
+		render :index
+	end
+
+	def create
+		post = current_user.posts.create(post_params)
+		redirect_to user_post_path
+	end
+
+	def new
+		@post = Post.new
+		render :new
+	end
+
+	def edit
+		render :edit
+	end
+
+	def show
+		@post = Post.find(params[:id])
+		render :show
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update_attributes(post_params)
+		redirect_to profile_path
+	end
+
+	def destroy
+		Post.find(params[:id]).destroy
+		redirect_to profile_path
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
+
+end
+
+#     user_posts GET    /users/:user_id/posts(.:format)          posts#index
+#                POST   /users/:user_id/posts(.:format)          posts#create
+#  new_user_post GET    /users/:user_id/posts/new(.:format)      posts#new
+# edit_user_post GET    /users/:user_id/posts/:id/edit(.:format) posts#edit
+#      user_post GET    /users/:user_id/posts/:id(.:format)      posts#show
+#                PATCH  /users/:user_id/posts/:id(.:format)      posts#update
+#                PUT    /users/:user_id/posts/:id(.:format)      posts#update
+#                DELETE /users/:user_id/posts/:id(.:format)      posts#destroy

175d9ce2d2b75bf5902c83c6b6a27d851db637e7
Erin Mahoney <erinmahoneyphoto@gmail.com>
2015-08-11 15:23:01 -0700

    styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810225312) do
+ActiveRecord::Schema.define(version: 20150811211159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "city_name"
+    t.string   "short_name"
+    t.string   "city_photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"

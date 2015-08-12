3ad5d786420b0d6ac5a5ecc6b3422d9e12b9cb54
henry-yi <yoonho.yi88@gmail.com>
2015-08-12 11:53:51 -0700

    add pagination

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,6 @@
 class CitiesController < ApplicationController
   def show
   	@city = City.find_by(short_name:params[:short_name])
-  	@posts = @city.posts.order(created_at: :desc)
+  	@posts = @city.posts.order(created_at: :desc).page(params[:page]).per(10)
   end
 end

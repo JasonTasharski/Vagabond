3e9e33e1b82e38dad14c69a8af92fde418a54d2a
henry-yi <yoonho.yi88@gmail.com>
2015-08-13 11:11:54 -0700

    fixed sessions bug and city_show bug no current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  # def current_user
+  # 	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+
   def current_user
-  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      if User.exists?(session[:user_id])
+        @current_user ||= User.find(session[:user_id])
+      else
+        session[:user_id] = nil
+      end
+    end
   end
 
   helper_method :current_user

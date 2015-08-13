73334d5a1b418790d30b201bdae7c72cb6ef09c8
J T <jtasharski@gmail.com>
2015-08-13 15:22:27 -0700

    More fixin's

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ActiveRecord::Base
                    :styles => { :medium => "150x150>", :thumb => "44x44#>" },
                    :default_url => "/images/:style/missing.png"
 
- validates_attachment :avatar, :presence => true,
-                      :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] },
-                      :size => { :in => 0..10.kilobytes }
+	validates_attachment :avatar, 
+                    	:content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] },
+                    	:size => { :in => 0..10.kilobytes }
 end
 
 	def to_param
